79fc56678cd2ad260a8ebc299c9d7c68689babcc
Kayla Cross <kac288@georgetown.edu>
2018-08-24 18:48:29 +0000

    automatically backed up by learn

--- models/course.rb
+++ models/course.rb
@@ -1,3 +1,17 @@
 class Course
 
+  attr_reader :name, :topic
+
+  COURSES = []
+
+  def initialize(params)
+    @name << params[:name]
+    @topic << params[:topic]
+    COURSES << self
+  end
+
+  def self.all
+    COURSES
+  end
+
 end
